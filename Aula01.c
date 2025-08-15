d992b3e7ccd7698c330334f313a543e6f7d98aa1
eduardomodolo <uniceantunes@fei.edu.br>
2025-08-14 21:52:59 -0300

    Update Aula01.c

---
+++ Aula01.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+ int main() {
+ int i = 1; // tipo inteiro
+ float f = 3.14; // ponto flutuante
+ char c = 'a';
+ printf("Inteiro: %d\n", i);
+ printf("Ponto flutuante: %f\n", f);
+ printf("Letra: %c\n", c);
+ return 0;
+ }
